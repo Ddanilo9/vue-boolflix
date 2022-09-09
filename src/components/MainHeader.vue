c7ee80e5c520922a1cb5a86e6c6887ef1ab5850d
Ddanilo9 <interiordanilo3@gmail.com>
2022-09-09 14:46:53 +0200

    first commit

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+   <h1>ciaonpm run serve
+
+   </h1>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+
+export default {
+ data(){
+  return{
+    movies: [],
+
+  }
+ }
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
